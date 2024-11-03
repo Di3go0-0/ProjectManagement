81066fee9f7bb7247a09161b6aaee1a37cbf4360
di3go0-0 <d.rincon@utp.edu.co>
2024-11-03 16:45:20 -0500

    First commit
    start aplication-backend to Project Management with ts, prismaORM
    
    Feat: create connection with mysql through prisma ORM

---
+++ prisma/migrations/20241103213153_task/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Taskk` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `done` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Taskk_title_key`(`title`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
