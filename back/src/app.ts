0760f75385193c3584164a327d3d363b976096e7
di3go0-0 <d.rincon@utp.edu.co>
2024-12-06 23:55:55 -0500

    update back

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import { taskRoutes, AuthRoutes, projectRoutes } from "./routes";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const clientURL = process.env.CLIENT_URL as string;
 
 const app = express();
-
+app.use(cors({
+  origin: clientURL,
+  credentials: true,
+}
+));
 app.use(express.json());
 app.use(cookieParser());
 
