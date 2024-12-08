116950fddf638a8fe3ec3f74ae418d628cc127ed
di3go0-0 <d.rincon@utp.edu.co>
2024-12-07 21:56:12 -0500

    update App

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,29 @@
 import './App.css'
-import { Register } from './Components'
+import { Register } from './Pages'
+import { AuthProvider } from './Context'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Login from './Pages/Public/Login/Login'
 
 function App() {
 
   return (
     <div className='Body'>
-      <Register />
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={
+              <Navigate to="/register" />
+            } />
+
+            < Route path="/register" element={<Register />} />
+            < Route path="/login" element={<Login />} />
+
+            <Route path="*" element={<Navigate to="/404" />} />
+            <Route path="/404" element={<h1>404 Not Found</h1>} />
+          </Routes>
+        </BrowserRouter>
+
+      </AuthProvider>
     </div>
   )
 }
