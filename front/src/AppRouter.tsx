9c2015876e9d816164d4974e080d3552ff6bc00d
di3go0-0 <d.rincon@utp.edu.co>
2024-12-09 21:09:20 -0500

    AppRouter

--- front/src/AppRouter.tsx
+++ front/src/AppRouter.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Register, Login } from "./Pages";
+import { BrowserRouter, Navigate, Route } from "react-router-dom";
 import { PrivateGuard } from "./Guard";
+import { PrivateRouter, PublicRoutes, RoutesWithNotFound } from "./Components";
 
 interface Props {
   children: ReactNode;
@@ -11,20 +11,15 @@ export const AppRouter = ({ children }: Props) => {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={
-            <Navigate to="/login" />
-          } />
-          <Route element={<PrivateGuard />} >
-            < Route path="/register" element={<Register />} />
-            < Route path="/login" element={<Login />} />
+        <RoutesWithNotFound>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/*" element={<PublicRoutes />} />
+          <Route element={<PrivateGuard />}>
+            <Route path="/private/*" element={<PrivateRouter />} />
           </Route>
-          <Route path="/home" element={<h1>Home</h1>} />
-
-          <Route path="*" element={<Navigate to="/404" />} />
-          <Route path="/404" element={<h1>404 Not Found</h1>} />
-        </Routes>
-      </BrowserRouter>
+        </RoutesWithNotFound>
+        {children}
+      </BrowserRouter >
     </>
   )
 }
