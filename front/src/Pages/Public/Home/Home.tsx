fafa632726618f4e1fba06a337d95cf0d4ded920
di3go0-0 <d.rincon@utp.edu.co>
2024-12-11 21:56:58 -0500

    Home page

--- front/src/Pages/Public/Home/Home.tsx
+++ front/src/Pages/Public/Home/Home.tsx
@@ -1,16 +1,34 @@
 import { useEffect } from "react"
-import { useAuth } from "../../../Context"
+import { useAuth, useProject } from "../../../Context"
+import './Home.css'
+import { Navbar, ProjectCard } from "../../../Components"
 
 
 export const Home = () => {
   const { Logout } = useAuth()
+  const { GetProjects, projects } = useProject()
 
+  useEffect(() => {
+    GetProjects()
+  }, [GetProjects])
+
+  // console.log(projects)
 
   return (
-    <div>
-      <h1>Homke</h1>
-
-      <button onClick={Logout}>Logout</button>
-    </div >
-  )
+    <>
+      <Navbar />
+      <div className='home-container'>
+        < h1 className='title' > Mis Proyectos</h1 >
+        <div className='project-cards'>
+          {projects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              Project={project}
+              taskCount={10}
+            />
+          ))}
+        </div>
+      </div >
+    </>
+  );
 }
