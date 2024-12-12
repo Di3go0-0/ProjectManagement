ce4a09514b184bec8c8257b03810c89bb178e90e
di3go0-0 <d.rincon@utp.edu.co>
2024-12-11 21:56:35 -0500

    Update indext to Project Context

--- front/src/Context/index.ts
+++ front/src/Context/index.ts
@@ -1,2 +1,6 @@
 export * from './AuthContext/AuthContext.tsx';
 export * from './AuthContext/Auth.Context.ts'
+
+export * from './ProjectContext/ProjectContext.ts'
+export * from './ProjectContext/ProjectProvider.tsx'
+
