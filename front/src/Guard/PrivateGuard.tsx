58203b88ac039322320d1dcabf3af4a02eb7f086
di3go0-0 <d.rincon@utp.edu.co>
2024-12-13 01:07:03 -0500

    update guard

--- front/src/Guard/PrivateGuard.tsx
+++ front/src/Guard/PrivateGuard.tsx
@@ -2,7 +2,11 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../Context";
 
 export const PrivateGuard = () => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <div>Loading...</div>; // O puedes mostrar un spinner de carga.
+  }
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
