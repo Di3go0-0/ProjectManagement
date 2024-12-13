7c0b2001eca2711adaf71805d725e10c68497dff
di3go0-0 <d.rincon@utp.edu.co>
2024-12-13 01:07:25 -0500

    update home

--- front/src/Pages/Public/Home/Home.tsx
+++ front/src/Pages/Public/Home/Home.tsx
@@ -1,29 +1,47 @@
-import { useProject } from "../../../Context"
 import './Home.css'
-import { ProjectCard } from "../../../Components"
-
+import { useProject } from "../../../Context"
+import { ProjectCard, ProjectManager } from "../../../Components"
+import { useEffect, useState } from "react"
 
 export const Home = () => {
-  const { GetProjects, projects } = useProject()
-  if (projects.length === 0) {
-    GetProjects()
-  }
-  console.log(projects)
+  const { GetProjects, projects } = useProject();
+  const [filter, setFilter] = useState<"all" | "active" | "complete">("all");
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  useEffect(() => {
+    GetProjects();
+  }, [GetProjects]);
+
+  // Filtrado de proyectos
+  const filteredProjects = projects.filter((project) => {
+    const titleMatches = project.title.toLowerCase().includes(searchQuery.toLowerCase());
+
+    const tasksPerforming = project.tasks.filter(task => task.done === true).length;
+    const tasksPending = project.tasks.filter(task => task.done === false).length;
+
+    if (filter === "all") return titleMatches;
+    if (filter === "active") return titleMatches && (tasksPending > 0 || project.tasks.length === 0);
+    if (filter === "complete") return titleMatches && tasksPending === 0 && tasksPerforming > 0;
+
+    return true;
+  });
 
   return (
     <>
-      <div className='home-container'>
-        < h1 className='title' > Mis Proyectos</h1 >
-        <div className='project-cards'>
-          {projects.map((project) => (
-            <ProjectCard
-              key={project.id}
-              Project={project}
-              taskCount={project.tasks.length}
-            />
+      <div className="app">
+        <ProjectManager
+          setFilter={setFilter}
+          filter={filter}
+          setSearchQuery={setSearchQuery}
+          searchQuery={searchQuery}
+        />
+        <main className="projects-grid">
+          {filteredProjects.map((project) => (
+            <ProjectCard key={project.id} Project={project} />
           ))}
-        </div>
-      </div >
+        </main>
+      </div>
     </>
   );
-}
+};
+
