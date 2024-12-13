6e264bc095fb9d742bb43f368d591db14db10c75
di3go0-0 <d.rincon@utp.edu.co>
2024-12-13 13:02:01 -0500

    update Description

--- front/src/Models/Project/Project.Model.ts
+++ front/src/Models/Project/Project.Model.ts
@@ -6,7 +6,7 @@ export const CreateProjectSchema = z
     title: z.string()
       .min(1, "Title is Required"),
     description: z.string()
-      .min(1, "Title is Required"),
+      .min(1, "Description is Required"),
   });
 
 export type CreateProjectValues = z.infer<typeof CreateProjectSchema>;
