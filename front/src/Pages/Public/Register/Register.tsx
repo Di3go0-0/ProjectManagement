57e91f4426f8a847d79096c4b0639681e253666a
di3go0-0 <d.rincon@utp.edu.co>
2024-12-13 12:59:06 -0500

    Register

--- front/src/Pages/Public/Register/Register.tsx
+++ front/src/Pages/Public/Register/Register.tsx
@@ -1,18 +1,18 @@
-import { useEffect } from 'react';
 import { RegisterForm } from '../../../Components';
 import { useAuth } from '../../../Context';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+// import { useEffect } from 'react';
 import '../Css/Auth.css'
 
 export const Register = () => {
-  const { SignUp, isRegistered, errors } = useAuth();
-  const navigate = useNavigate();
+  const { SignUp, errors } = useAuth();
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    if (isRegistered) {
-      navigate("/login");
-    }
-  }, [isRegistered, navigate]);
+  // useEffect(() => {
+  //   if (isRegistered) {
+  //     navigate("/login");
+  //   }
+  // }, [isRegistered, navigate]);
 
 
   return (
