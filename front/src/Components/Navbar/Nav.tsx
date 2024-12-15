84d61bc397f6ec95facaf843859214cff5ee8fc1
di3go0-0 <d.rincon@utp.edu.co>
2024-12-14 20:56:33 -0500

    update navbar

--- front/src/Components/Navbar/Nav.tsx
+++ front/src/Components/Navbar/Nav.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './nav.css';
 import { useAuth } from '../../Context';
+import { Link } from 'react-router-dom';
 
 export const Navbar: React.FC = () => {
   const { user, Logout } = useAuth();
@@ -10,9 +11,14 @@ export const Navbar: React.FC = () => {
     <nav className="navbar">
       <div className="navbar-container">
         <div className="navbar-content">
-          <div className="navbar-user">
-            <h1>Wellcome </h1>
-            <h1>{" " + user?.name}</h1>
+          {/* <div className="navbar-user"> */}
+          {/*   <h1>Wellcome </h1> */}
+          {/*   <h1>{" " + user?.name}</h1> */}
+          {/* </div> */}
+          <div className="links-container">
+            <Link to="/private/home" className="link">
+              Home
+            </Link>
           </div>
           <button className="logout-button" onClick={Logout}>
             Logout
