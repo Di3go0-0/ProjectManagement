76f847c308c047f91b6ef23da8fef892ef2b1bef
di3go0-0 <d.rincon@utp.edu.co>
2024-12-16 22:57:59 -0500

    upate Guard Project

--- front/src/Components/Routes/PrivateRouter/PrivateRouter.tsx
+++ front/src/Components/Routes/PrivateRouter/PrivateRouter.tsx
@@ -2,13 +2,18 @@ import { Route } from "react-router-dom"
 import { RoutesWithNotFound } from "../RoutesWithNotFound/RoutesWithNotFound"
 import { Home, ProjectPage } from "../../../Pages"
 import { Navbar } from "../../Navbar/Nav"
+import { ProjectGuard } from "../../../Guard"
 
 export const PrivateRouter = () => {
   return (
     <>
       <Navbar />
       <RoutesWithNotFound>
-        <Route path="/project/:id" element={<ProjectPage />} />
+        <Route path="/project/:id" element={
+          <ProjectGuard>
+            <ProjectPage />
+          </ProjectGuard>
+        } />
         <Route path="/home" element={<Home />} />
         < Route path="/tasks" element={< h1 > Tasks</h1 >} />
       </RoutesWithNotFound >
