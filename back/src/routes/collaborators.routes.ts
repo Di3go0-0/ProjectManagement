80ba9a74193b265f667151a103a849ffb622f5e7
di3go0-0 <d.rincon@utp.edu.co>
2024-12-21 23:12:35 -0500

    update new endpoints

--- back/src/routes/collaborators.routes.ts
+++ back/src/routes/collaborators.routes.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import { userToken, validateSchema } from "../middleware";
-import { addProjectCollaborator } from "../controllers";
+import {
+  addProjectCollaborator,
+  getProjectCollaborators
+} from "../controllers";
 
 const router = Router();
 
 
 router.post("/project", userToken, addProjectCollaborator);
+router.get("/project", userToken,
+  (req, res) => { res.json({ message: "Task collaborator added" }) }
+  // getProjectCollaborators
+);
+router.post("/task", userToken, (req, res) => { res.json({ message: "Task collaborator added" }) });
 
 export const collaboratorRoutes = (app: any) => app.use("/collaborator", router);
+
