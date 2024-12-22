6afcb895b8d9e1799a657a43b6c74acad1cf9e54
di3go0-0 <d.rincon@utp.edu.co>
2024-12-22 12:53:55 -0500

    update collaborator repository

--- back/src/repository/collaborator.repository.ts
+++ back/src/repository/collaborator.repository.ts
@@ -1,10 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 import type { IProject, IUser } from "../interfaces";
+import { getUserId } from "../helpers";
 
 const prisma = new PrismaClient();
 
 interface addCollaboratorProps {
   projectId: number;
+  userId: number;
+}
+
+interface isTaskCollaborator {
+  taskId: number;
+  cookie: string;
+}
+
+interface addTaskCollaborator {
+  taskId: number;
   userId: number;
 }
 
@@ -48,6 +59,23 @@ export const isProjectCollaboratorRepo = async ({ projectId, userId }: addCollab
   return !!collaborator;
 }
 
+export const isTaskCollaboratorRepo = async ({ taskId, cookie }: isTaskCollaborator): Promise<boolean> => {
+  try {
+    const userId = await getUserId(cookie);
+    if (!userId) return false;
+    const collaborator = await prisma.taskColaborator.findFirst({
+      where: {
+        taskId,
+        userId,
+      },
+    });
+
+    return !!collaborator;
+  } catch {
+    return false;
+  }
+}
+
 
 export const getCollaboratorsByProjectId = async (projectId: number): Promise<IUser[] | null> => {
   try {
@@ -66,3 +94,19 @@ export const getCollaboratorsByProjectId = async (projectId: number): Promise<IU
   }
 };
 
+
+export const addTaskCollaboratorRepo = async ({ taskId, userId }: addTaskCollaborator): Promise<boolean> => {
+  try {
+    const task = await prisma.taskColaborator.create({
+      data: {
+        taskId,
+        userId,
+      },
+    });
+
+
+    return !!task;
+  } catch {
+    return false;
+  }
+}
