bb243b8b5dce094996c34735a63eb3c960f9481c
di3go0-0 <d.rincon@utp.edu.co>
2024-12-23 18:39:09 -0500

    update collaborator controller

--- back/src/controllers/collaborator.controller.ts
+++ back/src/controllers/collaborator.controller.ts
@@ -1,34 +1,30 @@
 import type { Request, Response } from "express";
-import {
-  addProjectCollaboratorRepo,
-  getCollaboratorsByProjectId,
-  isProjectCollaboratorRepo,
-  isUserProjectOwner,
-  obtainUserByIdRepo
-} from "../repository";
+import { getCollaboratorsByProjectId, isUserProjectOwner } from "../repository";
 
 
 
-
-export const getProjectCollaborators = async (req: Request, res: Response) => {
-  const { projectId } = req.body;
+export const getCollaboratorsProject = async (req: Request, res: Response) => {
+  const { projectId } = req.query;
   const cookie = req.cookies.token as string;
 
   try {
-    // const isOwner = await isUserProjectOwner(projectId as number, cookie);
-    // if (!isOwner) res.status(404).json({ message: "Project not found" });
-    //
-    // const collaborators = await getCollaboratorsByProjectId(projectId as number);
-    //
-    // if (!collaborators) res.status(404).json({ message: "Collaborators not found" });
-    //
-    // res.status(200).json(collaborators);
+    if (!projectId) res.status(400).json({ message: "Project ID is required" });
 
-    res.status(200).json({ message: "Collaborators found" });
+
+    const projectIdNumber = Number(projectId);
+    if (isNaN(projectIdNumber)) res.status(400).json({ message: "Project ID must be a number" });
+
+
+    const project = await isUserProjectOwner(projectIdNumber, cookie);
+    if (!project) res.status(404).json({ message: "Project not found" });
+
+    const collaborators = await getCollaboratorsByProjectId(projectIdNumber);
+    if (!collaborators) res.status(404).json({ message: "Collaborators not found" });
+
+
+    res.status(200).json(collaborators);
 
   } catch (e) {
-    res.status(500).json({
-      message: "Internal server error",
-    });
+    res.status(500).json({ message: "Internal server error" });
   }
 }
