9d1055557f6b3c5b37e522c9a53d201dab9be232
di3go0-0 <d.rincon@utp.edu.co>
2024-12-25 18:45:18 -0500

    update swagger dots

--- back/src/swaggerDots/collaboratorSwaggerDots.ts
+++ back/src/swaggerDots/collaboratorSwaggerDots.ts
@@ -7,14 +7,14 @@ export const collaboratorSwaggerDocs = {
   ],
 
   paths: {
-    "/collaborator/project": {
+    "/collaborator/project/{projectId}": {
       get: {
         summary: "Get all collaborators of a project",
         tags: ["Collaborator"],
         parameters: [
           {
             name: "projectId",
-            in: "query",
+            in: "path",
             required: true,
             schema: {
               type: "number",
@@ -31,6 +31,55 @@ export const collaboratorSwaggerDocs = {
         },
       },
     },
+    "/collaborator/user/project/{userId}": {
+      get: {
+        summary: "Get all projects where a user is a collaborator",
+        tags: ["Collaborator"],
+        parameters: [
+          {
+            name: "userId",
+            in: "path",
+            required: true,
+            schema: {
+              type: "number",
+            },
+          },
+        ],
+        responses: {
+          200: {
+            description: "Collaborators found",
+          },
+          404: {
+            description: "Collaborators not found",
+          },
+        },
+      },
+    },
+    "/collaborator/user/task/{projectId}": {
+      get: {
+        summary: "Get all tasks where a user is a collaborator",
+        tags: ["Collaborator"],
+        parameters: [
+          {
+            name: "projectId",
+            in: "path",
+            required: true,
+            schema: {
+              type: "number",
+            },
+          },
+        ],
+        responses: {
+          200: {
+            description: "Collaborators found",
+          },
+          404: {
+            description: "Collaborators not found",
+          },
+        },
+      },
+    },
+
     "/collaborator/task": {
       post: {
         summary: "Add a collaborator to a task",
