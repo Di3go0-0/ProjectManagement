67f05eeaf4147df0df398e0a03b8a50b0e35400d
di3go0-0 <d.rincon@utp.edu.co>
2024-12-25 18:45:11 -0500

    update routes

--- back/src/routes/collaborators.routes.ts
+++ back/src/routes/collaborators.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { userToken, validateSchema } from "../middleware";
 import {
+  getAllProjectsForCollaborator,
+  getAllTasksForCollaborator,
   getCollaboratorsProject,
 } from "../controllers";
 
@@ -8,9 +10,9 @@ const router = Router();
 
 
 
-router.get("/project", userToken, getCollaboratorsProject);
-
-
+router.get("/project/:projectId", userToken, getCollaboratorsProject);
+router.get("/user/project/:userId", userToken, getAllProjectsForCollaborator);
+router.get("/user/task/:projectId", userToken, getAllTasksForCollaborator);
 router.post("/task", userToken, (req, res) => { res.json({ message: "Task collaborator addeda" }) });
 
 export const collaboratorRoutes = (app: any) => app.use("/collaborator", router);
