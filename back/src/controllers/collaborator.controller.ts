8ef41c8dccbae8740645066dfbdc881371d0ec8f
di3go0-0 <d.rincon@utp.edu.co>
2024-12-25 18:44:41 -0500

    Get all projects weher a user is collaborador and the same but with the tasks

--- back/src/controllers/collaborator.controller.ts
+++ back/src/controllers/collaborator.controller.ts
@@ -1,10 +1,10 @@
 import type { Request, Response } from "express";
-import { getCollaboratorsByProjectId, isUserProjectOwner } from "../repository";
+import { getCollaboratorsByProjectId, getProjectsForCollaboratorRepo, getTasksForCollaboratorRepo, isUserProjectOwner, obtainUserByIdRepo } from "../repository";
 
 
 
 export const getCollaboratorsProject = async (req: Request, res: Response) => {
-  const { projectId } = req.query;
+  const { projectId } = req.params;
   const cookie = req.cookies.token as string;
 
   try {
@@ -27,4 +27,40 @@ export const getCollaboratorsProject = async (req: Request, res: Response) => {
   } catch (e) {
     res.status(500).json({ message: "Internal server error" });
   }
+}
+
+export const getAllProjectsForCollaborator = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  try {
+    if (!userId) res.status(400).json({ message: "User ID is required" });
+
+    const user = await obtainUserByIdRepo(Number(userId));
+    if (!user) res.status(404).json({ message: "User not found" });
+
+    const projects = await getProjectsForCollaboratorRepo(Number(userId));
+    if (!projects) res.status(404).json({ message: "Projects not found" });
+
+    res.status(200).json(projects);
+  } catch (e) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export const getAllTasksForCollaborator = async (req: Request, res: Response) => {
+  const { projectId } = req.params;
+  const cookie = req.cookies.token as string;
+
+  try {
+    if (!projectId) res.status(400).json({ message: "project ID is required" });
+
+
+    const tasks = await getTasksForCollaboratorRepo({ projectId: Number(projectId), cookie });
+    if (!tasks) res.status(404).json({ message: "Projects not found" });
+
+    res.status(200).json(tasks);
+  } catch (e) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+
 }
